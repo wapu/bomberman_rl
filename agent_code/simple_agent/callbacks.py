1bb305fc17169bab16dc2d956007c8b83f65e6d2
Jakob Kruse <jakob.kruse@iwr.uni-heidelberg.de>
2019-01-15 14:25:56 +0100

    Improvements to baseline agent

--- agent_code/simple_agent/callbacks.py
+++ agent_code/simple_agent/callbacks.py
@@ -1,50 +1,55 @@
 
 import numpy as np
 from random import shuffle
-from queue import PriorityQueue
-from time import sleep
+from time import time, sleep
+from collections import deque
+
+from settings import s
 
 
-def a_star_search(free_space, start, goal):
-    frontier = PriorityQueue()
-    frontier.put(start, 0)
-    parent_dict = {start: None}
-    cost_dict = {start: 0}
-
+def look_for_targets(free_space, start, targets):
+    frontier = [start]
+    parent_dict = {start: start}
     best = start
-    best_dist = abs(start[0] - goal[0]) + abs(start[1] - goal[1])
+    best_dist = np.sum(np.abs(np.subtract(targets, start)), axis=1).min()
 
-    while not frontier.empty():
-        current = frontier.get()
-        if current == goal: break
+    while len(frontier) > 0:
+        current = frontier.pop(0)
+
+        d = np.sum(np.abs(np.subtract(targets, current)), axis=1).min()
+        if d <= best_dist:
+            best = current
+            best_dist = d
+        if d == 0: break
 
         x, y = current
         neighbors = [(x,y) for (x,y) in [(x+1,y), (x-1,y), (x,y+1), (x,y-1)] if free_space[x,y]]
-
         for n in neighbors:
-            cost_n = cost_dict[current] + 1
-            if n not in cost_dict or cost_n < cost_dict[n]:
-                cost_dict[n] = cost_n
-                heuristic = abs(n[0] - goal[0]) + abs(n[1] - goal[1])
-                if heuristic < best_dist:
-                    best = n
-                    best_dist = heuristic
-                frontier.put(n, cost_n + heuristic)
+            if n not in parent_dict:
+                frontier.append(n)
                 parent_dict[n] = current
 
-    p = best
-    if p == start: return start
+    current = best
     while True:
-        if parent_dict[p] == start: return p
-        p = parent_dict[p]
+        if parent_dict[current] == start: return current
+        current = parent_dict[current]
 
 
 def setup(agent):
     agent.logger.debug('Successfully entered setup code')
     np.random.seed()
+    agent.history = deque(['WAIT',])
 
+
+from numpy.linalg import inv
 def act(agent):
     agent.logger.info('Picking action according to rule set')
+
+    # if np.random.rand() > 0.9:
+    #     # waste time
+    #     t = time()
+    #     while time() - t < s.timeout:
+    #         foo = inv(np.random.rand(1000,1000))
 
     # Get info
     arena = agent.game_state['arena']
@@ -76,22 +81,40 @@ def act(agent):
     if bombs_left > 0: valid_actions.append('BOMB')
     agent.logger.debug(f'Valid actions: {valid_actions}')
 
+    # Check for cycles in previous behaviour to avoid repetition in the late game
+    # NEEDS TO BE BETTER
+    last_action = agent.history[0]
+    if len(agent.history) > 40:
+        try:
+            prev_occurrence = agent.history.index(last_action, 1)
+            if prev_occurrence < len(agent.history) / 2:
+                length = max(prev_occurrence, 20)
+                cycle = True
+                for i in range(1, length):
+                    if agent.history[i] != agent.history[prev_occurrence + i]:
+                        cycle = False
+                        break
+                if cycle:
+                    # Disallow the action that followed after the last time
+                    a = agent.history[prev_occurrence - 1]
+                    agent.logger.debug(f'Encountered cycle of length {length}, avoid action {a}')
+                    if a in valid_actions:
+                        valid_actions.remove(a)
+        except ValueError:
+            # Action never occurred before, no problem
+            pass
+
     # Collect actions in a queue
     action_ideas = ['UP', 'DOWN', 'LEFT', 'RIGHT']
     shuffle(action_ideas)
 
     # Head towards nearest coin or opponent
-    xn, yn = min(others+coins, key=lambda xy: abs(xy[0] - x) + abs(xy[1] - y))
-    d = a_star_search(arena == 0, (x,y), (xn,yn))
+    d = look_for_targets(arena == 0, (x,y), others+coins)
     if d == (x,y-1): action_ideas.append('UP')
     if d == (x,y+1): action_ideas.append('DOWN')
     if d == (x-1,y): action_ideas.append('LEFT')
     if d == (x+1,y): action_ideas.append('RIGHT')
-    # if (yn < y): action_ideas.append('UP')
-    # if (yn > y): action_ideas.append('DOWN')
-    # if (xn < x): action_ideas.append('LEFT')
-    # if (xn > x): action_ideas.append('RIGHT')
-    # if abs(yn-y) > abs(xn-x): action_ideas.append(action_ideas[-2])
+    if d == (x,y): action_ideas.append('BOMB')
 
     # Place bomb if at dead end or next to opponent
     if len(valid) == 2 or (min(abs(xy[0] - x) + abs(xy[1] - y) for xy in others)) <= 1:
@@ -119,10 +142,14 @@ def act(agent):
         a = action_ideas.pop()
         if a in valid_actions:
             agent.next_action = a
-            return
+            break
+
+    agent.history.appendleft(agent.next_action)
+
 
 def reward_update(agent):
     agent.logger.debug(f'Found reward of {agent.reward}')
+
 
 def end_of_episode(agent):
     agent.logger.debug(f'Found final reward of {agent.reward}')
