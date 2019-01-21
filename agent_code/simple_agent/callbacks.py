927d5250c4294cb5f770c11679c71fbb06dab266
Jakob Kruse <jakob.kruse@iwr.uni-heidelberg.de>
2019-01-21 19:52:11 +0100

    Added some docstrings

--- agent_code/simple_agent/callbacks.py
+++ agent_code/simple_agent/callbacks.py
@@ -8,6 +8,18 @@ from settings import s
 
 
 def look_for_targets(free_space, start, targets):
+    """Find direction of closest target that can be reached via free tiles.
+
+    Performs a breadth-first search of the reachable free tiles until a target is encountered.
+    If no target is found, the tile that is closest to any target is chosen.
+
+    Args:
+        free_space: Boolean numpy array. True for free tiles and False for obstacles.
+        start: the coordinate from which to begin the search.
+        targets: list or array holding the coordinates of all target tiles.
+    Returns:
+        coordinate of first step towards closest target or towards tile closest to any target.
+    """
     frontier = [start]
     parent_dict = {start: start}
     best = start
@@ -15,20 +27,20 @@ def look_for_targets(free_space, start, targets):
 
     while len(frontier) > 0:
         current = frontier.pop(0)
-
+        # Find distance from current position to all targets, track closest
         d = np.sum(np.abs(np.subtract(targets, current)), axis=1).min()
         if d <= best_dist:
             best = current
             best_dist = d
         if d == 0: break
-
+        # Add unexplored free neighboring tiles to the queue
         x, y = current
         neighbors = [(x,y) for (x,y) in [(x+1,y), (x-1,y), (x,y+1), (x,y-1)] if free_space[x,y]]
-        for n in neighbors:
-            if n not in parent_dict:
-                frontier.append(n)
-                parent_dict[n] = current
-
+        for neighbor in neighbors:
+            if neighbor not in parent_dict:
+                frontier.append(neighbor)
+                parent_dict[neighbor] = current
+    # Determine the first step towards the best found target tile
     current = best
     while True:
         if parent_dict[current] == start: return current
@@ -36,22 +48,35 @@ def look_for_targets(free_space, start, targets):
 
 
 def setup(agent):
+    """Called once before a set of games to initialize data structures etc.
+
+    The 'agent' object passed to this method will be the same in all other
+    callback methods. You can assign new properties (like agent.history below)
+    here or later on and they will be persistent even across multiple games.
+    You can also use the agent.logger object at any time to write to the log
+    file for debugging (see https://docs.python.org/3.7/library/logging.html).
+    """
     agent.logger.debug('Successfully entered setup code')
     np.random.seed()
     agent.history = deque(['WAIT',])
 
 
-from numpy.linalg import inv
 def act(agent):
+    """Called each game step to determine the agent's next action.
+
+    You can find out about the state of the game environment via agent.game_state,
+    which is a dictionary. Consult 'get_state_for_agent' in environment.py to see
+    what it contains.
+
+    Set the action you wish to perform by assigning the relevant string to
+    agent.next_action. You can assign to this variable multiple times during
+    your computations. If this method takes longer than the time limit specified
+    in settings.py, execution is interrupted by the game and the current value
+    of agent.next_action will be used. The default value is 'WAIT'.
+    """
     agent.logger.info('Picking action according to rule set')
 
-    # if np.random.rand() > 0.9:
-    #     # waste time
-    #     t = time()
-    #     while time() - t < s.timeout:
-    #         foo = inv(np.random.rand(1000,1000))
-
-    # Get info
+    # Gather information about the game state
     arena = agent.game_state['arena']
     x, y, _, bombs_left = agent.game_state['self']
     bombs = agent.game_state['bombs']
@@ -64,7 +89,7 @@ def act(agent):
             if (0 < i < bomb_map.shape[0]) and (0 < j < bomb_map.shape[1]):
                 bomb_map[i,j] = min(bomb_map[i,j], t)
 
-    # Check which moves make sense
+    # Check which moves make sense at all
     directions = [(x,y), (x+1,y), (x-1,y), (x,y+1), (x,y-1)]
     valid, valid_actions = [], []
     for d in directions:
@@ -81,8 +106,8 @@ def act(agent):
     if bombs_left > 0: valid_actions.append('BOMB')
     agent.logger.debug(f'Valid actions: {valid_actions}')
 
-    # Check for cycles in previous behaviour to avoid repetition in the late game
-    # NEEDS TO BE BETTER
+    # Check for cycles in previous behaviour, remove repeating actions from
+    # valid actions list (this is not ideal yet)
     last_action = agent.history[0]
     if len(agent.history) > 40:
         try:
@@ -104,11 +129,12 @@ def act(agent):
             # Action never occurred before, no problem
             pass
 
-    # Collect actions in a queue
+    # Collect basic action proposals in a queue
+    # Later on, the last added action that is also valid will be chosen
     action_ideas = ['UP', 'DOWN', 'LEFT', 'RIGHT']
     shuffle(action_ideas)
 
-    # Head towards nearest coin or opponent
+    # Add action proposal to head towards nearest coin or opponent
     d = look_for_targets(arena == 0, (x,y), others+coins)
     if d == (x,y-1): action_ideas.append('UP')
     if d == (x,y+1): action_ideas.append('DOWN')
@@ -116,40 +142,55 @@ def act(agent):
     if d == (x+1,y): action_ideas.append('RIGHT')
     if d == (x,y): action_ideas.append('BOMB')
 
-    # Place bomb if at dead end or next to opponent
+    # Add proposal to drop a bomb if at dead end or next to opponent
     if len(valid) == 2 or (min(abs(xy[0] - x) + abs(xy[1] - y) for xy in others)) <= 1:
         action_ideas.append('BOMB')
 
-    # Run away from any dangerous bomb
+    # Add proposal to run away from any nearby bomb about to blow
     for xb,yb,t in bombs:
         if (xb == x) and (abs(yb-y) < 4):
             # Run away
             if (yb > y): action_ideas.append('UP')
             if (yb < y): action_ideas.append('DOWN')
-            # if possible turn a corner
+            # If possible, turn a corner
             action_ideas.append('LEFT')
             action_ideas.append('RIGHT')
         if (yb == y) and (abs(xb-x) < 4):
             # Run away
             if (xb > x): action_ideas.append('LEFT')
             if (xb < x): action_ideas.append('RIGHT')
-            # if possible turn a corner
+            # If possible, turn a corner
             action_ideas.append('UP')
             action_ideas.append('DOWN')
 
-    # Pick first action from queue that is valid
+    # Pick last action added to the proposals list that is also valid
     while len(action_ideas) > 0:
         a = action_ideas.pop()
         if a in valid_actions:
             agent.next_action = a
             break
 
+    # Keep track of chosen action for cycle detection
     agent.history.appendleft(agent.next_action)
 
 
 def reward_update(agent):
+    """Called once per step to allow intermediate rewards based on game events.
+
+    When this method is called, agent.events will contain a list of all game
+    events relevant to your agent that occured during the previous step. Consult
+    settings.py to see what events are tracked. You can hand out rewards to your
+    agent based on these events and your knowledge of the (new) game state. In
+    contrast to act, this method has no time limit.
+    """
     agent.logger.debug(f'Encountered {len(agent.events)} game events.')
 
 
 def end_of_episode(agent):
+    """Called at the end of each game to hand out final rewards and do training.
+
+    This is similar to reward_update, except it is only called at the end of a
+    game. agent.events will contain all events that occured during your agent's
+    final step. You should place your actual learning code in this method.
+    """
     agent.logger.debug(f'Encountered {len(agent.events)} game events in final step.')
--- agents.py
+++ agents.py
@@ -11,8 +11,8 @@ from items import *
 from settings import s, e
 
 
-# Context manager to protect code from badly timed Interrupts
 class IgnoreKeyboardInterrupt(object):
+    """Context manager that protects enclosed code from Interrupt signals."""
     def __enter__(self):
         self.old_handler = signal.signal(signal.SIGINT, self.handler)
     def handler(self, sig, frame):
@@ -22,6 +22,7 @@ class IgnoreKeyboardInterrupt(object):
 
 
 class AgentProcess(mp.Process):
+    """Wrapper class that runs custom agent code in a separate process."""
 
     def __init__(self, pipe_to_world, ready_flag, name, agent_dir, train_flag):
         super(AgentProcess, self).__init__(name=name)
@@ -31,7 +32,7 @@ class AgentProcess(mp.Process):
         self.train_flag = train_flag
 
     def run(self):
-        # Set up logging
+        # Set up individual loggers for the wrapper and the custom code
         self.wlogger = logging.getLogger(self.name + '_wrapper')
         self.wlogger.setLevel(s.log_agent_wrapper)
         self.logger = logging.getLogger(self.name + '_code')
@@ -45,7 +46,7 @@ class AgentProcess(mp.Process):
         self.wlogger.addHandler(handler)
         self.logger.addHandler(handler)
 
-        # Import custom code for the agent
+        # Import custom code for the agent from provided script
         self.wlogger.info(f'Import agent code from "agent_code/{self.agent_dir}/callbacks.py"')
         self.code = importlib.import_module('agent_code.' + self.agent_dir + '.callbacks')
 
@@ -58,7 +59,7 @@ class AgentProcess(mp.Process):
         self.wlogger.debug('Set flag to indicate readiness')
         self.ready_flag.set()
 
-        # Repeat until global exit message is received
+        # Play one game after the other until global exit message is received
         while True:
             # Receive round number and check for exit message
             self.wlogger.debug('Wait for new round')
@@ -68,9 +69,9 @@ class AgentProcess(mp.Process):
                 break
             self.wlogger.info(f'STARTING ROUND #{self.round}')
 
-            # Repeat until exit message for this round is received
+            # Take steps until exit message for current round is received
             while True:
-                # Receive new world state and check for exit message
+                # Receive new game state and check for exit message
                 self.wlogger.debug('Receive game state')
                 self.game_state = self.pipe_to_world.recv()
                 if self.game_state['died']:
@@ -114,7 +115,7 @@ class AgentProcess(mp.Process):
                     self.wlogger.debug('Set flag to indicate readiness')
                     self.ready_flag.set()
 
-            # Learn from episode if in training mode
+            # Process final events and learn from episode if in training mode
             if self.train_flag.is_set():
                 self.wlogger.info('Finalize agent\'s training')
                 self.wlogger.debug('Receive final event queue')
@@ -132,8 +133,10 @@ class AgentProcess(mp.Process):
 
 
 class Agent(object):
+    """Class representing agents as game objects."""
 
     def __init__(self, process, pipe_to_agent, ready_flag, color, train_flag):
+        """Set up agent, process for custom code and inter-process communication."""
         self.name = process.name
         self.process = process
         self.pipe = pipe_to_agent
@@ -142,14 +145,14 @@ class Agent(object):
         self.train_flag = train_flag
         self.events = []
 
-        # Load custom avatar or robot avatar of assigned color
+        # Load custom avatar or standard robot avatar of assigned color
         try:
             self.avatar = pygame.image.load(f'agent_code/{self.process.agent_dir}/avatar.png')
             assert self.avatar.get_size() == (30,30)
         except Exception as e:
             self.avatar = pygame.image.load(f'assets/robot_{self.color}.png')
 
-        # Prepare overlay to indicate dead agent on the scoreboard
+        # Prepare overlay that will indicate dead agent on the scoreboard
         self.shade = pygame.Surface((30,30), SRCALPHA)
         self.shade.fill((0,0,0,208))
 
@@ -164,6 +167,7 @@ class Agent(object):
         self.reset()
 
     def reset(self):
+        """Make agent ready for a new game round."""
         self.times = []
         self.mean_time = 0
         self.dead = False
@@ -172,17 +176,20 @@ class Agent(object):
         self.bombs_left = 1
 
     def get_state(self):
-        # return ((self.x, self.y), self.bomb_timer, self.explosion_timer, self.bomb_power, self.bombs_left, self.name)
+        """Provide information about this agent for the global game state."""
         return (self.x, self.y, self.name, self.bombs_left)
 
     def update_score(self, delta):
+        """Add delta to both the current round's score and the total score."""
         self.score += delta
         self.total_score += delta
 
     def make_bomb(self):
+        """Create a new Bomb object at current agent position."""
         return self.bomb_type((self.x, self.y), self, self.bomb_timer, self.bomb_power, self.color)
 
     def render(self, screen, x, y):
+        """Draw the agent's avatar to the screen at the given coordinates."""
         screen.blit(self.avatar, (x, y))
         if self.dead:
             screen.blit(self.shade, (x, y))
