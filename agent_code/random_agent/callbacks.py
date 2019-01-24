daee94861c4951e06976074ce5dd048a4a9691bf
Jakob Kruse <jakob.kruse@iwr.uni-heidelberg.de>
2019-01-24 17:52:20 +0100

    Random agent has small chance to drop bomb

--- agent_code/random_agent/callbacks.py
+++ agent_code/random_agent/callbacks.py
@@ -1,21 +1,16 @@
 
 import numpy as np
-from time import sleep
 
 
 def setup(agent):
-    agent.logger.debug('Successfully entered setup code')
     np.random.seed()
 
 def act(agent):
     agent.logger.info('Pick action at random')
-    # if np.random.rand() > 0.9:
-    #     sleep(2.1)
-    # sleep(0.1 * np.random.rand())
-    agent.next_action = np.random.choice(['RIGHT', 'LEFT', 'UP', 'DOWN'])
+    agent.next_action = np.random.choice(['RIGHT', 'LEFT', 'UP', 'DOWN', 'BOMB'], p=[.23, .23, .23, .23, .08])
 
 def reward_update(agent):
-    agent.logger.debug(f'Found reward of {agent.reward}')
+    pass
 
 def end_of_episode(agent):
-    agent.logger.debug(f'Found final reward of {agent.reward}')
+    pass
